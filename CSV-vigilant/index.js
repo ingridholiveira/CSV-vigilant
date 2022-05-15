86e33961cf95773518f140dabd101785f41ea795
Ingridh Oliveira <ingridhnunes@gmail.com>
2022-05-15 19:23:50 -0300

    Funções de adição e formatação e comentários

--- CSV-vigilant/index.js
+++ CSV-vigilant/index.js
@@ -3,9 +3,10 @@ const { parse } = require('fast-csv');
 const fs = require('fs');
 const validator = require("node-email-validation");
 
-
-const stream = fs.createReadStream("input1.csv");
+const stream = fs.createReadStream("input.csv");
 let dataCsv = [];
+
+//Lendo um CSV.
 const streamCsv = parse()
     .on('error', error => console.error(error))
     .on('data', data => {
@@ -16,6 +17,7 @@ const streamCsv = parse()
         promiseWriteJson(dataCsv)
     });
 
+//Converte e formata os dados fornecidos em uma linha do CSV em JSON.
 const convertDataJson = (row, header) => {
     let objJson = {
         "fullname": '',
@@ -33,22 +35,43 @@ const convertDataJson = (row, header) => {
             case 'eid':
                 objJson.eid = row[j];
                 break;
-            case 'invisible':
-                objJson.invisible = row[j];
+            case 'invisible':{
+                if (row[j] == 'yes' || row[j] == "1" || row[j] == 'true')objJson.invisible = true;
+                else objJson.invisible = false;
                 break;
-            case 'see_all':
-                objJson.see_all = row[j];
+            }
+            case 'see_all':{
+                if (row[j] == 'yes' || row[j] == "1" || row[j] == 'true')objJson.see_all = true;
+                else objJson.see_all = false; 
                 break;
-            case 'group':
-                objJson.groups.push(row[j]);
+            }
+            case 'group':{
+                if (formatValuesGroups(row[j]) != null ){
+                    objJson.groups=objJson.groups.concat(formatValuesGroups(row[j]));
+                }
                 break;
+            }
             default: {
-                let stringAdrress = header[j].split(" ");
-                objJson.addresses.push({
-                    "type": stringAdrress.shift(),
-                    "tags": stringAdrress,
-                    "address": formatValues(row[j])
-                });
+                const value = formatValues(row[j]);
+                if (value != null ){
+                    let stringAdrress = header[j].split(" ");
+                    let stringType = stringAdrress.shift();
+                    if (Array.isArray(value)){
+                        for(let i=0; i<value.length; i++){
+                            objJson.addresses.push({
+                            "type": stringType,
+                            "tags": stringAdrress,
+                            "address": value[i]
+                        });                         
+                        }
+                    } else {
+                            objJson.addresses.push({
+                            "type": stringType,
+                            "tags": stringAdrress,
+                            "address": value
+                        });
+                    }
+                }
             }
             break;
         }
@@ -56,6 +79,8 @@ const convertDataJson = (row, header) => {
     return objJson;
 }
 
+
+//Valida e formata os dados fornecidos de email e telefone.
 const formatValues = (data) => {
     try {
         const number = phoneUtil.parseAndKeepRawInput(data, 'BR');
@@ -63,13 +88,29 @@ const formatValues = (data) => {
             return number.getCountryCode() + '' + number.getNationalNumber();
         }
     } catch(e){}
-    if (validator.is_email_valid(data)){
-        return data;
-    }
+    try{
+        const email = ((data.match(/([a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9_-]+)/gi)));
+        let returnEmail = [];
+        for (let i =0; i < email.length; i++){
+            if (validator.is_email_valid(email[i])){
+                returnEmail.push(email[i]);
+            }
+        }        
+        if (returnEmail.length>0 && returnEmail[0] != "") return returnEmail;
+    } catch(e){}
     return null;
 }
 
 
+//Formata e devolve um array com os grupos.
+const formatValuesGroups = (data) => {
+	let stringGroups = data.split(/[,.\/\-]+/);
+	if(stringGroups[0] != '') return stringGroups;
+	return null;
+}
+
+//Executada na conclusão de leitura de um CSV.
+//Responsável pela conversão de um CSV em JSON e identificar objeto existente para atualização ou inserção.
 const promiseWriteJson = (dataCsv) => {
     let header = dataCsv[0];
     let listObjJson = [];
@@ -95,17 +136,6 @@ const promiseWriteJson = (dataCsv) => {
     });
 }
 
-
+//Start do processo
 stream.pipe(streamCsv);
 console.log(dataCsv);
-
-//parse({
-//    headers: true - não funciona pois da erro duplicate groups
-//})
-//.on('error', error => console.error(error))
-//.on('data', row => console.log(row))
-//.on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
-
-//stream.write('header1,header2\n');
-//stream.write('col1,col2');
-//stream.end();
